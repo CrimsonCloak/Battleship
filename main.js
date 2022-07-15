72bffb2a3502978c76c02c142334e6c33f196abf
Crimson_Cloak <alexander.veldeman.999@gmail.com>
2022-07-15 16:27:47 +0200

    Added full test layout for ships, added noise on hit (very interesting ones, might I add...)

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const explanation = document.getElementById("explanation");
 const returnbutton = document.getElementById("return");
 const tdcollection = [].slice.call(document.getElementsByTagName("td")); //document.getelements returns htmlcollection, this is a way to make that an array!
 let shotsText = document.getElementById("shots");
-
+let music; let boosh1; let boosh2;
 
 
 
@@ -67,12 +67,10 @@ start.onclick = () =>
 
 
     function setUpMusic() {
-        startmenu.insertAdjacentHTML("afterbegin",
-            "\n" +
-            "<audio id='music' autoplay=\"autoplay\">\n" +
-            "    <source src=\"sounds/Funky%20music.mp3\" />\n" +
-            "</audio>"
-        )
+        music = new Audio("sounds/Funky_music.mp3");
+        music.play();
+        boosh1 = new Audio("sounds/Boosh.mp3")
+        boosh2 = new Audio("sounds/Actually_strong_boosh.mp3");
     }
 
     function gameOver() {
@@ -102,6 +100,7 @@ start.onclick = () =>
 
             if (ships[i].indexOf(target) !== -1) {
                 hit = true;
+                Math.random() < 0.5 ? boosh1.play(): boosh2.play();
                 giveFeedback("You hit!")
                 tile.style.backgroundColor = "green";
                 tile.disabled;
@@ -109,10 +108,9 @@ start.onclick = () =>
                 /*ships[i].splice(ships[i].indexOf(target), 1);*/
                 console.log(ships[i].splice(ships[i].indexOf(target), 1));
 
-                if(ships[i].length === 0)
+                if(ships[i].length === 0) {
                     ships.splice(ships.indexOf(ships[i]), 1);
-                console.log(`Destroyed a ship! ${ships.length} left to destroy!`)
-
+                }
                 if (ships.length === 0)
                     gameWon();
 
@@ -134,23 +132,44 @@ start.onclick = () =>
 
     function placeShips() {
  /*       ships:
-       1x6
-       2x4
-       3x3
-       4x2
-
-
        IDEE VOOR SCHEPEN
        Kies richting boot (horizontaal/verticaal)
        Kies random rij/kolom en begin met de grote boot (6)
        Doe checks of dit past (randen, collisions met andere boten,...)
        "Plaats" finaal de boot, maak er een ship-variabele van en push die dan in de ships array
         */
-        let ship1 = ["1A","2A","3A","4A"];
+   /*   let ship1 = ["1A","2A","3A","4A"];
         let ship2 = ["1B","2B","3B","4B"];
         let ship3 = ["1I","1J"];
-        let ship4 = ["7C","7D","7E"]
-        ships.push(ship1,ship2, ship3, ship4);
+        let ship4 = ["7C","7D","7E"]*/
+
+        /*let carrier = ["I2","I3","I4","I5","I6"];
+        let battleship1 = ["C1","C2","C3","C4"];
+        let battleship2 = ["G8","H8","I8","J8"];
+        let destroyer1 = ["J4","J5","J6"]
+        let destroyer2 = ["F6","F7","F8"]
+        let destroyer3 = ["A6","B6","C6"]
+        let submarine1 = ["E2","E3","E4"]
+        let submarine2 = ["A8","B8","C8"]
+        let submarine3 = ["F1","G1","H1"]
+        let submarine4 = ["E5","F5","G5"]
+        let patrolboat1 = ["B1","B2"]
+        let patrolboat2 = ["G3","H3"]*/
+       /* ships.push(carrier,battleship1,battleship2);*/
+        ships.push(
+            ["2I","3I","4I","5I","6I"],
+            ["1C","2C","3C","4C"],
+            ["8G","8H","8I","8J"],
+            ["4J","5J","6J"],
+            ["6F","7F","8F"],
+            ["6A","6B","6C"],
+            ["2E","3E","4E"],
+            ["8A","8B","8C"],
+            ["1F","1G","1H"],
+            ["5E","5F","5G"],
+            ["1B","2B"],
+            ["3G","3H"]
+        )
         console.log(ships)
 
     //occupiedTiles variable will be used to track which tiles are already taken in the generation of ships!
